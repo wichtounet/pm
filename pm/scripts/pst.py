f4af99fced262f377b6e6a3b36b5e5c0e3ab6ffe
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-29 17:59:08 +0200

    Prepare pls script

---
+++ pm/scripts/pst.py
@@ -0,0 +1,31 @@
+import argparse
+import sys
+import pm
+
+from pm.projects import list_projects
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('--version',
+                        action='version',
+                        version='%(prog)s ' + pm.VERSION)
+
+    return parser
+
+
+def main(args=None):
+    # Parse command line
+    parser = build_parser()
+    args = parser.parse_args(args)
+
+    projects = list_projects(False)
+
+    for p in projects:
+        print(p)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
