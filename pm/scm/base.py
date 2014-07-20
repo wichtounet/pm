9654132dc319c0dbe0ac55a0ea337bf31415bb54
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-20 17:56:14 +0200

    Add license

--- pm/scm/base.py
+++ pm/scm/base.py
@@ -1,3 +1,10 @@
+#=======================================================================
+# Copyright (c) 2014 Baptiste Wicht
+# Distributed under the terms of the MIT License.
+# (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+#=======================================================================
+
 class subproject:
     def __init__(self, name):
         self.name = name
---
+++ pm/scripts/__init__.py
@@ -0,0 +1,8 @@
+#=======================================================================
+# Copyright (c) 2014 Baptiste Wicht
+# Distributed under the terms of the MIT License.
+# (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+#=======================================================================
+
+
