89b49efde943595235c0fe3211e11f6b1137abdd
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-20 17:32:44 +0200

    Cleanup

--- setup.py
+++ setup.py
@@ -37,15 +37,15 @@ class InstallCommand(install):
 
 setup(
         name='pm',
-        version='0.1.1',
+        version='0.1',
         description='Command-line utility to mange development projects',
         author='Baptiste Wicht',
         author_email='baptiste.wicht@gmail.com',
         url='https://github.com/wichtounet/pm',
-        download_url='https://github.com/wichtounet/pm/tarball/0.0.1',
+        download_url='https://github.com/wichtounet/pm/tarball/0.1',
         license='MIT',
         packages=find_packages(),
-        package_dir = {'pytex': 'pytex'},
+        package_dir = {'pm': 'pm'},
         entry_points = read('entry-points.ini'),
         cmdclass={
             'install': InstallCommand,
